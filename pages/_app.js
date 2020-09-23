5275caf38924f43d41eacbd9b92ed45fdf50c672
Adam Jermstad <acc@amjerm.me>
2020-09-23 14:58:37 -0500

    feat(build): Adding tailwindcss

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
-import '../styles/globals.css'
+import '../styles.css'
 
-function MyApp({ Component, pageProps }) {
+function App ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
+export default App
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+import Head from 'next/head'
 
 export default function Home () {
   return (
-    <div>
+    <div id='container'>
+      <Head>
+        <title>Adam Jermstad</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <header>
         <h1>Adam Jermstad</h1>
+        <h6>Web Developer in Austin, TX</h6>
       </header>
       <div className='body' />
       <footer>
